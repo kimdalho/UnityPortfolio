e5e42452031414c22bc50f2224c50bf754af924c
kimdalho <49323810+kimdalho@users.noreply.github.com>
2025-04-08 20:15:09 +0900

    쉐이더 추가

--- Assets/Scripts/RoomGrid/GridRow.cs
+++ Assets/Scripts/RoomGrid/GridRow.cs
@@ -12,5 +12,42 @@ public class GridRow : MonoBehaviour
     { 
         return listGrid;
     }
+    public void RepositionChildren()
+    {
+        int count = transform.childCount;
 
+        for (int i = 0; i < count; i++)
+        {
+            Transform child = transform.GetChild(i);
+            child.localPosition = new Vector3(i, 0, 0); // (0,0,0), (1,0,0), (2,0,0) ...
+        }
+
+        Debug.Log("자식 객체 위치 재배치 완료");
+    }
+
+    public void SetList()
+    {
+        listGrid.Clear();
+        int count = transform.childCount;
+
+        for (int i = 0; i < count; i++)
+        {
+            Transform child = transform.GetChild(i);
+            GridNode node =  child.GetComponent<GridNode>();
+            if (node != null)
+            {
+                listGrid.Add(node);
+            }
+        }
+
+        Debug.Log("자식 객체 위치 재배치 완료");
+    }
+
+    public void CreateWall()
+    {
+        foreach (GridNode node in listGrid)
+        {
+            node.CreateWall();
+        }
+    }
 }
