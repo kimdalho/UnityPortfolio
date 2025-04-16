aa3e923413e6612d18ddbc4e6576fdeed5a2f945
LMS <jin721003@naver.com>
2025-04-16 17:49:33 +0900

    BazookaMissile 오브젝트 추가 및 Projectile 오브젝트에 Trail Effect 추가
    1. BazookaMissile
    - gravity 적용하여 포물선을 그리며 Move
    - 특정 오브젝트와 접촉 시 폭발하는 FX 및 폭발 Detect 추가
    - ability 시전자도 공격 받을 수 있는 부분은 추후 추가예정

---
+++ Assets/Scripts/GAS/Projectile/BazookaMissile.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class BazookaMissile : Projectile
+{
+    [SerializeField] private float explosionRange = 3f;
+
+    protected override void CreateDetectObject(float detectSize)
+    {
+        var _hits = SphereDetector.DetectObjectsInSphere(transform.position, detectSize, targetLayer);
+        if (_hits == null || _hits.Length.Equals(0)) return;
+
+        ReleaseProjectile();
+    }
+
+    protected override void ReleaseProjectile()
+    {
+        FXFactory.Instance.GetFX("Explosion", transform.position, Quaternion.identity);
+
+        var _hits = SphereDetector.DetectObjectsInSphere(transform.position, explosionRange, targetLayer);
+        foreach (var _hit in _hits)
+        {
+            if (_hit.TryGetComponent<AttributeEntity>(out var _ae))
+            {
+                var _effect = new GameEffect(new DamageExecution());
+                _effect.Apply(owner, _ae);
+                (_ae as Character)?.fxSystem?.ExecuteFX(abilityTag);
+            }
+        }
+
+        base.ReleaseProjectile();
+    }
+}
